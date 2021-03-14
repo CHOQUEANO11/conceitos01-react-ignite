272b0b4845895f55fe56b705c240272ff05fb71a
cmtgeralpmpa <pmpa123456>
2021-03-14 16:21:37 -0300

    desafio01-ingnite-react-finish

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,9 @@ export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="/logo.svg" alt="to.do"/>
+        <h3 style={{color: 'white', fontSize: 30, margin: 20}}>Nilson</h3>
+        <img style={{width: 200, height: 200, borderRadius: 100}} src="/yo.jpg" />
+        <h3 style={{color: 'white', margin: 20, fontSize: 30}}>Developer</h3>
       </div>
     </header>
   )
